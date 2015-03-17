1416ba096522b8d9942a572134ce0219b2125222
laysejuju <laysejuju@gmail.com>
2015-03-17 16:26:30 -0300

    gerador-caos

---
+++ input.js
@@ -0,0 +1,18 @@
+﻿function variar(){
+	var variacaoBolsa = Math.round(Math.random() * (10 -1)+1);
+	console.log(variacaoBolsa);
+	
+	if(variacaoBolsa <= 4){
+		document.getElementById("valorVariacao").innerHTML ="Queda de " + "</br>" + "<span>" + "-"+ variacaoBolsa;
+	}
+	
+	if(variacaoBolsa > 4){
+		document.getElementById("valorVariacao").innerHTML ="Ganho de "  + "</br>" + "<span>" + "+"+ variacaoBolsa;
+	}
+	if(variacaoBolsa == 5){
+		document.getElementById("valorVariacao").innerHTML ="Bolsa equilibrada";
+	}
+}
+
+
+
