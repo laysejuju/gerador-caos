92fbdc4b269aecda71d650d9b1a5c54ba0d76205
laysejuju <laysejuju@gmail.com>
2015-03-19 14:26:19 -0300

    Atualização

--- input.js
+++ input.js
@@ -6,12 +6,30 @@
 		document.getElementById("valorVariacao").innerHTML ="Queda de " + "</br>" + "<span>" + "-"+ variacaoBolsa;
 	}
 	
-	if(variacaoBolsa > 4){
-		document.getElementById("valorVariacao").innerHTML ="Ganho de "  + "</br>" + "<span>" + "+"+ variacaoBolsa;
-	}
 	if(variacaoBolsa == 5){
 		document.getElementById("valorVariacao").innerHTML ="Bolsa equilibrada";
 	}
+	
+	if(variacaoBolsa == 6){
+		document.getElementById("valorVariacao").innerHTML ="Ganho de "  + "</br>" + "<span>" + "+1";
+	}
+	
+	if(variacaoBolsa == 7){
+		document.getElementById("valorVariacao").innerHTML ="Ganho de "  + "</br>" + "<span>" + "+2";
+	}
+	
+	if(variacaoBolsa == 8){
+		document.getElementById("valorVariacao").innerHTML ="Ganho de "  + "</br>" + "<span>" + "+3";
+	}
+	
+	if(variacaoBolsa == 9){
+		document.getElementById("valorVariacao").innerHTML ="Ganho de "  + "</br>" + "<span>" + "+4";
+	}
+	
+	if(variacaoBolsa == 10){
+		document.getElementById("valorVariacao").innerHTML ="Ganho de "  + "</br>" + "<span>" + "+5";
+	}
+
 }
 
 
